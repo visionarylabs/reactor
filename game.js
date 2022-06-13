7b6b31c1ec3b271e43f4d03541e11e64a4e1e9a5
ben <ben@opalgames>
2022-06-12 21:30:03 -0700

    rn2 basic grid code

---
+++ game.js
@@ -0,0 +1,110 @@
+/**
+    STATE FOR TRIAL OF TIM EDITOR
+    Editor Board State and Options
+**/
+// game setup options
+var options = {
+    tileSize : 50,
+    columns : 9,
+    rows : 9,
+    gameSpeed : 500,
+    gameDelay : 400,
+    renderTimer : false,
+    renderStatic : false,
+    tileActionColor : "rgba(255, 255, 255, 0)", //"rgba(255, 255, 255, 0.4)"
+    tileHoverColor : "rgba(252, 251, 177, 0.25)" //"rgba(252, 251, 177, 0.5)"
+};
+
+/**
+    MAIN GAME FACTORY
+    make all the game pieces
+    game piece templates
+**/
+var gameFactory = function () {
+
+    const tileFactory = function(num,col,row,tileType,x,y,color) {
+        var tile = {
+            num : num,
+            col : col,
+            row : row,
+            tileType : tileType,
+            contains : [], // reference to sprite in game list
+            ui : {
+                x : x,
+                y : y,
+                color : color,
+            }
+        };
+        tile.ui = setUi(x,y,options.tileSize,options.tileSize,color,0,0,0);
+        return tile;
+    };
+    
+    /**
+        board factory for TOT level editor
+    **/
+    const boardFactory = function(cols,rows) {
+        var board = [];
+        var tile = null;
+        var iCol = 0;
+        var iRow = 0;
+        var num = 0;
+        var color = null;
+        var tileType = null;
+        
+        for( iRow; iRow < rows; iRow++ ) {
+            for( iCol; iCol < cols; iCol++ ) {
+                num++;
+                if( options.columns % 2 == 0 ) {
+                    if( iRow % 2 == 0 ) {
+                        if( num % 2 == 0 ) color = '#aaa'; else color = '#bbb';
+                    }else{
+                        if( num % 2 == 0 ) color = '#bbb'; else color = '#aaa';
+                    }
+                }else{
+                    if( num % 2 == 0 ) color = '#aaa'; else color = '#bbb';
+                }
+                
+                tileType = 'tile';
+                
+                tile = tileFactory( num, iCol, iRow, tileType, iCol * options.tileSize, iRow * options.tileSize, color );
+                tile.action = true; //all true for editor
+                board.push(tile);
+            }
+            iCol = 0;
+        }
+        
+        return board;
+    }
+    
+    /**
+        box is the full data modal of all rules / monster tables decks etc
+    **/
+    var getBox = function () {
+
+        var board = new boardFactory(options.columns,options.rows);
+
+        //return the game box
+        return {
+            board : board
+        }
+        
+    }
+    
+    var setUi = function (x,y,w,h,color,ox,oy,io){
+        return{
+            x : x,
+            y : y,
+            size : {w:w,h:h},
+            color : color,
+            image : new Image(),
+            shapeOffset : {x:ox,y:oy},
+            imageOffset : io,
+        }
+    }
+    
+    return {
+        getBox : getBox,
+        setUi : setUi,
+    }
+}
+//end gameFactory OBJ
---
+++ render.js
@@ -0,0 +1,194 @@
+/**
+    OPAL GAMES
+    VIEW / RENDER
+    Just setup and render loop
+    assumes box has sprites and board
+    rules is loaded with functions
+        general game render loop
+            handle delta time updates
+            draw board and sprite list from state file
+        test with:
+            weird quest
+            away team
+            beryillum
+            --eventually use for:
+                death dome
+                monster hunter
+
+    TODO:
+        seperate out modules for
+            board grid render
+            UI render
+                menu / score board etc
+**/
+
+//global vars
+var w = window;
+var d = document;
+var mouse = {x:0,y:0};
+
+//main toolkit vars
+var game = null;
+var box = null;
+var state = null;
+var rules = null;
+var dataUrl = null;
+
+//interface / timer / canvas
+var curTime = 0;
+var then = null;
+var startTime = null;
+var canvas = null;
+var ctx = null;
+
+/****
+    INTERFACE AND CANVAS RENDER
+    NO GAME LOGIC SHOULD BE HERE
+    JUST VISUAL & USER INPUT / USER INTERFACE
+****/
+
+var ogRender = function(){
+
+    //TIMER SET UP
+    then = Date.now();
+    startTime = Date.now();
+
+    // create the canvas
+    canvas = document.getElementById('game-canvas');
+    ctx = canvas.getContext("2d");
+    canvas.width = options.columns * options.tileSize;
+    canvas.height = options.rows * options.tileSize;
+    canvas.id = 'game-canvas';
+
+    canvas.addEventListener('mousemove', function(e) {
+        mouse = getMousePos(canvas,e);
+    });
+
+    canvas.addEventListener('mouseout', function(e) {
+        mouse = {x:-1,y:-1};
+    });
+
+    canvas.addEventListener('click', function(e) {
+        click = getMousePos(canvas,e);
+        rules.processClick(click);
+    });
+
+    function getMousePos(canvas,e) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+          x: Math.floor(e.clientX - rect.left),
+          y: Math.floor(e.clientY - rect.top)
+        };
+    }
+
+    var init = function () {
+        render();
+    };
+
+    // Update game objects
+    // Ran from loop with delta modifier
+    var update = function (modifier) {
+        var i = null;
+        var loopLength = null;
+        var size = null;
+        var color = null;
+        var node = null;
+        var tempPos = null;
+        var gameSpeed = options.gameSpeed;
+        var spriteSnap = 2;
+        var gameMod = gameSpeed * modifier;
+    };
+
+    // RENDERING
+
+    // Draw everything // Ran from loop
+    var render = function () {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        renderBoard();
+        renderTimer();
+        if(options.renderTimer) renderTimer();
+        if(options.renderStatic) renderStatic();
+    };
+
+    // draw the board
+    var renderBoard = function () {
+        var i = 0;
+        var loopLength = board.length;
+        var tile = null;
+        for(i; i < loopLength; i++) {
+            tile = board[i];
+            //draw the tile
+            ctx.fillStyle = tile.ui.color;
+            ctx.fillRect(tile.ui.x,tile.ui.y,options.tileSize,options.tileSize);
+            //see if the mouse if hovering draw a hover
+            if(
+                tile.ui.x < mouse.x &&
+                tile.ui.y < mouse.y &&
+                tile.ui.x >= mouse.x - options.tileSize &&
+                tile.ui.y >= mouse.y - options.tileSize &&
+                tile.action === true
+            ){
+                ctx.fillStyle = '#fcfbb1';
+                ctx.fillStyle = options.tileHoverColor;
+                ctx.fillRect(tile.ui.x,tile.ui.y,options.tileSize,options.tileSize);
+            }
+        }
+    }
+
+    // Render Static Elements
+    var renderStatic = function () {
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.font = "18px Helvetica";
+        ctx.textAlign = "left";
+        ctx.textBaseline = "top";
+
+        if( options.gameName ){
+            ctx.fillText(options.gameName, 10, 10);
+        }
+
+        if( options.levelName ){
+            ctx.fillText(options.levelName, 300, 10);
+        }
+
+        if(state.state.currentSelection){
+            ctx.fillText(state.state.currentSelection, 110, (options.rows * options.tileSize - options.tileSize) );
+        }
+        ctx.fillText(state.state.history[state.state.history.length-1], 210, (options.rows * options.tileSize - options.tileSize) );
+    }
+
+    // Render Timer
+    var renderTimer = function () {
+        ctx.fillStyle = "rgb(255, 255, 255)";
+        ctx.font = "18px Helvetica";
+        ctx.textAlign = "left";
+        ctx.textBaseline = "top";
+        ctx.fillText("Time: " + curTime, 10, (options.rows * options.tileSize - options.tileSize) );
+        //ctx.fillText("Mouse: " + mouse.x + ", " + mouse.y, 300, 470);
+    }
+
+    // The main game loop
+    var loop = function () {
+        var now = Date.now();
+        var delta = now - then;
+        update(delta / 1000);
+        render();
+        then = now;
+        curTime = Math.floor( (then - startTime) / 1000 );
+        requestAnimationFrame(loop);
+    };
+
+    // Cross-browser support
+    requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+
+    return{
+        init : init,
+        loop : loop
+    }
+}
+
+var gf = new gameFactory();
+var ren = new ogRender();
+var box = gf.getBox();
+var board = box.board;
+ren.init();
+ren.loop();
